54945d0d19cd0071678ac26285b51dff36e6acd2
Marcin <markar.mormar@gmail.com>
2018-05-19 14:50:58 +0200

    Add star rating and comment all console.log

--- js/app.js
+++ js/app.js
@@ -4,17 +4,17 @@ const cardLenght = document.querySelectorAll('.card').length;
 let picture = ['fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-cube', 'fa fa-leaf',
 'fa fa-bicycle', 'fa fa-bomb','fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-cube',
 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-bomb'];
-console.log(picture);
+//console.log(picture);
 let randomSort = picture.sort(() => 0.5 - Math.random());
-console.log(picture);
-console.log(randomSort);
+//console.log(picture);
+//console.log(randomSort);
 
 if( cardLenght != 0) {
   for(let idCounter = 0; idCounter < cardLenght; idCounter++) {
     $(".card.cursor-point"+idCounter).attr("id", idCounter);
     $(".card.cursor-point"+idCounter).next().addClass("cursor-point"+(idCounter+1));
     $(".card.cursor-point"+idCounter).removeClass("cursor-point"+idCounter);
-     console.log(document.getElementById(idCounter));
+    // console.log(document.getElementById(idCounter));
   }
 }
 let pictureCounter;
@@ -22,7 +22,7 @@ for(pictureCounter = 0; pictureCounter < cardLenght; pictureCounter++){
   $(".addCard"+pictureCounter).addClass(picture[pictureCounter]);
   $(".addCard"+pictureCounter).next().addClass("addCard"+(pictureCounter+1));
   $(".addCard"+pictureCounter).removeClass("addCard"+pictureCounter);
-  console.log(picture[pictureCounter]);
+  //console.log(picture[pictureCounter]);
 }
 
 // Correct Guess
@@ -40,45 +40,64 @@ function turn(){
 
 $(".card").click(function(event)
 {
-  console.log("number = " + number);
+  if(moveCounter < 12) {
+    console.log("less than 12 moves");
+  }
+  else if(moveCounter < 18) {
+    console.log("less than 18 moves");
+    $("#star3").removeClass();
+    $("#star3").addClass("fa fa-star-o");
+  }
+  else if(moveCounter < 24) {
+    console.log("less than 24 moves");
+    $("#star2").removeClass();
+    $("#star2").addClass("fa fa-star-o");
+  }
+  else {
+    console.log("More then 24!");
+    $("#star1").removeClass();
+    $("#star1").addClass("fa fa-star-o");
+  }
+  //console.log("number = " + number);
   if (number < 2) {
     number++;
+
     if(number === 1) {
       firstCard = event.target.childNodes[1];
-      console.log("firstCard " + firstCard);
+      //console.log("firstCard " + firstCard);
       firstId = document.getElementById(event.target.id);
-      console.log("firstId " + firstId);
+      //console.log("firstId " + firstId);
       $(firstId).addClass("open show");
-      console.log("addClass open show to firstId");
+      //console.log("addClass open show to firstId");
     }
     else {
       secondCard = event.target.childNodes[1];
-      console.log("secondCard " + secondCard);
+      //console.log("secondCard " + secondCard);
       secondId = document.getElementById(event.target.id);
-      console.log("secondId " + secondId);
+      //console.log("secondId " + secondId);
       $(secondId).addClass("open show");
-      console.log("addClass open show to secondId");
+      //console.log("addClass open show to secondId");
     }
 
     if(number === 2) {
       if(firstCard.isEqualNode(secondCard)) {
-        console.log("firstCard and secondCard are equal");
-        console.log( "document.getElementById(event.target.id) " + document.getElementById(event.target.id));
+        //console.log("firstCard and secondCard are equal");
+        //console.log( "document.getElementById(event.target.id) " + document.getElementById(event.target.id));
         $(firstId).addClass("match");
         $(secondId).addClass("match");
         $(firstId).removeClass("open show");
         $(secondId).removeClass("open show");
-        console.log($("#proba").addClass("match"));
+        //console.log($("#proba").addClass("match"));
         number = 0;
         firstCard = "";
         secondCard = "";
-        console.log("number = " + number);
+        //console.log("number = " + number);
         moveCounter++;
-        console.log(moveCounter);
+        //console.log(moveCounter);
         $(".moves").html(moveCounter);
       }
       else {
-        console.log("firstCard and secondCard are different");
+        //console.log("firstCard and secondCard are different");
         if(!(firstCard.isEqualNode(secondCard))) {
         $(firstId).addClass("wrong");
         $(secondId).addClass("wrong");
@@ -87,7 +106,7 @@ $(".card").click(function(event)
         firstCard = "";
         secondCard = "";
         moveCounter++;
-        console.log(moveCounter);
+        //console.log(moveCounter);
         $(".moves").html(moveCounter);
         }
       }
@@ -100,16 +119,16 @@ $(".restart").click(function() {
     number = 0;
     firstCard = "";
     secondCard = "";
-    console.log("restart moveCounter = " + moveCounter);
-    console.log("restart number = " + number);
-    console.log("restart firstCard = " + firstCard);
-    console.log("restart secondCard = " + secondCard);
+    //console.log("restart moveCounter = " + moveCounter);
+    //console.log("restart number = " + number);
+    //console.log("restart firstCard = " + firstCard);
+    //console.log("restart secondCard = " + secondCard);
     $(".moves").html(moveCounter);
     $(".card").removeClass("open show");
     $(".card").removeClass("match");
     randomSort = picture.sort(() => 0.5 - Math.random());
-    console.log(picture);
-    console.log(randomSort);
+    //console.log(picture);
+    //console.log(randomSort);
     for(let deletId = 0; deletId < cardLenght; deletId++) {
     $("#"+deletId).children().removeClass();
     $("#"+deletId).children().addClass("addCard"+deletId);
@@ -119,6 +138,12 @@ $(".restart").click(function() {
       $(".addCard"+pictureCounter).addClass(picture[pictureCounter]);
       $(".addCard"+pictureCounter).next().addClass("addCard"+(pictureCounter+1));
       $(".addCard"+pictureCounter).removeClass("addCard"+pictureCounter);
-      console.log(picture[pictureCounter]);
+      //console.log(picture[pictureCounter]);
     }
+    $("#star1").removeClass();
+    $("#star2").removeClass();
+    $("#star3").removeClass();
+    $("#star1").addClass("fa fa-star");
+    $("#star2").addClass("fa fa-star");
+    $("#star3").addClass("fa fa-star");
 });
